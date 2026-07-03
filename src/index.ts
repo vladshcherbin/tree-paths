09c296cc6af22476e90be2872152be0192ce8147
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2026-07-03 04:29:17 +0300

    Add TreeNode type export

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import createTree from './create-tree.ts'
+import createTree, { type TreeNode } from './create-tree.ts'
 import sortPaths from './sort-paths.ts'
 
 function treeifyPaths(paths: string[], parentPath = '') {
   return createTree(sortPaths(paths), parentPath)
 }
 
-export { sortPaths, treeifyPaths }
+export { sortPaths, treeifyPaths, type TreeNode }
